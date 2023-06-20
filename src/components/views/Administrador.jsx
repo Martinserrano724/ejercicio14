2cb289793cbd2886fba1d36e1e0f4f87bbaeb5eb
Martinserrano724 <martinserrano724@gmail.com>
2023-06-20 14:40:52 -0300

    correccion administrador

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import ItemProducto from "./producto/ItemProducto";
 const Administrador = () => {
   return (
-    <div className="container  my-5">
-        <div className="d-flex my-3">
-      <h3>Productos disponibles</h3>
-      
-      <Button variant="primary" className='ms-auto btnAgregar'>Agregar</Button>{' '}</div>
-      <hr />
-      <Table striped bordered hover>
-        <thead>
-          <tr>
+   
+    <section className="container mainSection">
+    <div className="d-flex justify-content-between align-items-center mt-5">
+      <h1 className="display-4 ">Productos disponibles</h1>
+      <Button className="btn btn-primary" to='/administrar/crear'>
+        Agregar
+      </Button>
+    </div>
+    <hr />
+    <Table responsive striped bordered hover>
+      <thead>
+      <tr>
             <th>Cod</th>
             <th>Receta</th>
             <th>Cant Platos</th>
@@ -19,43 +23,12 @@ const Administrador = () => {
             <th className="text-truncate">Ingrediente</th>
             <th>Opciones</th>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Pulpo a la plancha</td>
-            <td>3</td>
-            <td>
-              <img
-                src="https://i.blogs.es/f0476b/pulpo-a-la-plancha-pakus-futurobloguero/1366_2000.jpg"
-                alt=""
-                className="imgAdministrador"
-              />
-            </td>
-            <td>
-              Pulpo cocido 3 tentáculos o patas Puré de patatas al gusto Dientes
-              de ajo para la ajada 2 Pimentón dulce para la ajada Aceite de
-              oliva virgen extra para la ajada, 4 cucharadas soperas Sal en
-              escamas para servir
-            </td>
-            <td>
-              <div>
-                {" "}
-                <Button variant="warning" className="ms-auto btnAgregar">
-                  Editar
-                </Button>{" "}
-              </div>
-              <div>
-                {" "}
-                <Button variant="danger" className="ms-auto btnAgregar">
-                  Borrar
-                </Button>{" "}
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+      </thead>
+      <tbody>
+       <ItemProducto></ItemProducto>
+      </tbody>
+    </Table>
+  </section>
   );
 };
 
--- src/components/views/producto/ItemProducto.jsx
+++ src/components/views/producto/ItemProducto.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 
 const ItemProducto = () => {
     return (
+      <tr>
+      <td>1</td>
+      <td>Pulpo a la plancha</td>
+      <td>3</td>
+      <td>
+        <img
+          src="https://i.blogs.es/f0476b/pulpo-a-la-plancha-pakus-futurobloguero/1366_2000.jpg"
+          alt=""
+          className="imgAdministrador"
+        />
+      </td>
+      <td>
+        Pulpo cocido 3 tentáculos o patas Puré de patatas al gusto Dientes
+        de ajo para la ajada 2 Pimentón dulce para la ajada Aceite de
+        oliva virgen extra para la ajada, 4 cucharadas soperas Sal en
+        escamas para servir
+      </td>
+      <td>
         <div>
-            
+          {" "}
+          <Button variant="warning" className="ms-auto btnAgregar">
+            Editar
+          </Button>{" "}
         </div>
+        <div>
+          {" "}
+          <Button variant="danger" className="ms-auto btnAgregar">
+            Borrar
+          </Button>{" "}
+        </div>
+      </td>
+    </tr>
     );
 };
 
