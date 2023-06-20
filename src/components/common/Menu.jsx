a4d1842d9f03b5c96c03e69910bd30b2f1f4909c
Martinserrano724 <martinserrano724@gmail.com>
2023-06-19 23:25:09 -0300

    Funcionamiento del Nav

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (
-        <div>
-            <Navbar  className='bgMenu text-bg-white' expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Recetas de la Abuela</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-        
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar className="bgMenu text-bg-white" expand="lg">
+        <Container>
+          <Navbar.Brand  as={Link} to={'/'}>Recetas de la Abuela</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-item nav-link">
+              <Nav.Link  as={Link} to={'/'} end>Inicio</Nav.Link>
+              <Nav.Link  as={Link} to={'/registro'}end >Registro</Nav.Link>
+              <Nav.Link as={Link} to={'/administrador'} end>Administrador</Nav.Link>
+              <Nav.Link  as={Link} to={'/login'} end>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Menu;
