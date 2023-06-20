1f4c9dfa39be2859d30c7de8e791df8a3d749be6
Martinserrano724 <martinserrano724@gmail.com>
2023-06-19 23:06:34 -0300

    terminado del maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,39 @@ import DettalleProducto from './components/views/DettalleProducto'
 import Error404 from './components/views/Error404'
 import CrearProducto from './components/views/producto/CrearProducto'
 import EditarProducto from './components/views/producto/EditarProducto'
+import Registro from './components/views/Registro'
+import Login from './components/views/Login'
 import Administrador from './components/views/Administrador'
+import Inicio from './components/views/Inicio'
+import {BrowserRouter ,Routes,Route} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Menu></Menu>
-   <Administrador></Administrador>
+    <BrowserRouter>
+    <Menu></Menu>
+      <Routes>
+        {
+          //Routes listado de routas
+        }
+        <Route exact path='/' element={<Inicio></Inicio>}/>
+        <Route exact path='/administrador' element={<Administrador></Administrador>}/>
+        <Route exact path='/registro' element={<Registro></Registro>}/>
+        <Route exact path='/login' element={<Login></Login>}/>
+        <Route exact path='/administrador' element={<Administrador></Administrador>}/>
+        <Route exact path='/administrador/crearProducto' element={<CrearProducto></CrearProducto>}/>
+        <Route exact path='/administrador/editarProducto' element={<EditarProducto></EditarProducto>}/>
+        <Route exact path='/detalleProducto' element={<DettalleProducto></DettalleProducto>}/>
+        <Route exact path='*' element={<Error404></Error404>}/>
+
+      </Routes>
       <Footer></Footer>
+    </BrowserRouter>
+    
+
+ 
     </>
   )
 }
--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import CardProducto from "./producto/CardProducto";
 
 const Inicio = () => {
     return (
         <div>
-            
+            <Carousel className=" ">
+        <Carousel.Item interval={3000} className="contenedorCarrusel">
+          <img
+            className="d-block  "
+            src="https://png.pngtree.com/thumb_back/fw800/back_our/20190619/ourmid/pngtree-fancy-coffee-background-material-image_134209.jpg"
+            alt="cafe"
+            width={"100%"}
+            height={"400px"}
+          />
+          
+        </Carousel.Item>
+        <Carousel.Item interval={3000}  className="contenedorCarrusel">
+          <img
+            className="d-block   "
+            src="https://apasionados-por-el-cafe.s3.amazonaws.com/wp-content/uploads/2020/10/15-09-BannersPreparacionesCoctelesCafe-Escritorio-Apasionados-1.png"
+            alt="Second slide"
+            width={"100%"}
+            height={"400px"}
+          />
+         
+        </Carousel.Item>
+        <Carousel.Item  className="contenedorCarrusel">
+          <img
+            className="d-block  "
+            src="https://st4.depositphotos.com/1835807/22599/i/600/depositphotos_225991768-stock-photo-coffee-banner-collage-with-espresso.jpg"
+            alt="Third slide"
+            width={"100%"}
+            height={"400px"}
+          />
+          
+        </Carousel.Item>
+      </Carousel>
+      <h3 className="text-center">Nuestros Productos</h3>
+      <section className="my-5 container d-flex justify-content-around "> <CardProducto></CardProducto></section>
         </div>
     );
 };
